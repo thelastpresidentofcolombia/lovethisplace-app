81a6d72e7ff020544202b2200627d38c4247c855
Juan Carlos Morales <morcaju@gmail.com>
2025-08-02 20:40:49 +0200

    fix: Final Vercel deployment configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import icon from "astro-icon";
-
-// --- ADD THIS LINE ---
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/serverless"; // This line was missing
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-
-  // --- ADD THIS LINE ---
-  adapter: vercel(),
-
+  adapter: vercel(), // This line was missing
   integrations: [
     tailwind(),
     icon()
